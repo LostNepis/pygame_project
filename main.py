d9a4f3a0d9f5977ca70963aac0431d6502191bed
dmitriy burya <dostupniy.nic@gmail.com>
2025-01-20 21:03:22 +0700

    создана генерация уровня

---
+++ main.py
@@ -0,0 +1,60 @@
+import random
+import pygame
+import sys
+
+# параметры
+size = width, height = 600, 600
+cell_size = 40
+
+
+def generate_level(level):
+    """Генерирует цветы и препятствия"""
+    flowers = []
+    obstacles = []
+    num_flowers = 5 + level # кол-во цветов
+    num_obstacles = 3 + level # кол-во стен
+
+    for i in range(num_flowers):
+        flowers.append((random.randint(0, 14), random.randint(0, 14)))  # добавляем цветы
+
+    for i in range(num_obstacles):
+        obstacles.append((random.randint(0, 14), random.randint(0, 14))) # добавляем препятствия
+
+    return flowers, obstacles
+
+def draw_level(screen, flowers, obstacles):
+    """Рисует уровень"""
+    screen.fill((255, 255, 255))
+    for x in range(0, width, cell_size):
+        pygame.draw.line(screen, (0, 0, 0), (x, 0), (x, height))
+    for y in range(0, height, cell_size):
+        pygame.draw.line(screen, (0, 0, 0), (0, y), (width, y))
+
+    for flower in flowers:
+        pygame.draw.rect(screen, (0, 255, 0), (flower[0] * cell_size, flower[1] * cell_size, cell_size, cell_size))
+    for obstacle in obstacles:
+        pygame.draw.rect(screen, (255, 0, 0), (obstacle[0] * cell_size, obstacle[1] * cell_size, cell_size, cell_size))
+
+    pygame.display.flip()
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("цветы")
+
+    level = 1
+
+    flowers, obstacles = generate_level(level)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        draw_level(screen, flowers, obstacles)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    sys.exit(main())
